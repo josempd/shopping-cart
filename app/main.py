3f1ad99874479b4c59815b7029d419241c82da6f
josempd <josempd@gmail.com>
2024-03-31 14:27:37 +0200

    added the items post rquest to allow the API to populate the database

--- app/main.py
+++ app/main.py
@@ -1,8 +1,23 @@
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
+from sqlalchemy.orm import Session
+
+from . import crud, schemas
+from .database import db_session
 
 app = FastAPI()
+
+
+def get_db():
+    with db_session() as db:
+        yield db
 
 
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
+
+
+@app.post("/items/", response_model=schemas.ItemDisplay)
+def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
+    db_item = crud.create_item(db=db, item=item)
+    return db_item
