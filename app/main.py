5055ee3b3bf3c4cfadba07367763559c6c1c95d5
josempd <josempd@gmail.com>
2024-03-31 17:24:16 +0200

    added all the crud operations and their endpoints

--- app/main.py
+++ app/main.py
@@ -24,6 +24,36 @@ def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
     return db_item
 
 
+@app.get("/items/{item_id}", response_model=schemas.ItemDisplay)
+def get_single_item(item_id: int, db: Session = Depends(get_db)):
+    item = crud.get_item(db=db, item_id=item_id)
+    if item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return item
+
+
+@app.get("/items/", response_model=list[schemas.ItemDisplay])
+def list_items(db: Session = Depends(get_db)):
+    items = crud.get_items(db=db)
+    return items
+
+
+@app.put("/items/{item_id}", response_model=schemas.ItemDisplay)
+def update_item(
+    item_id: int, item_update: schemas.ItemUpdate, db: Session = Depends(get_db)
+):
+    updated_item = crud.update_item(db=db, item_id=item_id, updated_item=item_update)
+    if updated_item is None:
+        raise HTTPException(status_code=404, detail="Item not found")
+    return updated_item
+
+
+@app.delete("/items/", status_code=200)
+def delete_items(request: schemas.ItemDeleteRequest, db: Session = Depends(get_db)):
+    crud.delete_items(db=db, item_ids=request.item_ids)
+    return {"message": "Item(s) deleted successfully"}
+
+
 @app.post("/carts/", response_model=schemas.CartDisplay)
 def create_cart(db: Session = Depends(get_db)):
     return crud.create_cart(db=db)
@@ -43,6 +73,13 @@ def read_cart(cart_id: int, db: Session = Depends(get_db)):
     return db_cart
 
 
+@app.post("/carts/{cart_id}/items", response_model=schemas.CartItemDisplay)
+def add_item_to_cart_endpoint(
+    cart_id: int, cart_item: schemas.CartItemCreate, db: Session = Depends(get_db)
+):
+    return crud.add_item_to_cart(db=db, cart_id=cart_id, cart_item_data=cart_item)
+
+
 @app.delete("/carts/{cart_id}")
 def delete_cart(cart_id: int, db: Session = Depends(get_db)):
     success = crud.delete_cart(db=db, cart_id=cart_id)
@@ -51,3 +88,18 @@ def delete_cart(cart_id: int, db: Session = Depends(get_db)):
     return JSONResponse(
         status_code=200, content={"message": "Cart deleted successfully"}
     )
+
+
+@app.delete("/carts/{cart_id}/items", status_code=200)
+def remove_item_from_cart_endpoint(
+    cart_id: int,
+    remove_request: schemas.CartItemRemoveRequest,
+    db: Session = Depends(get_db),
+):
+    crud.remove_item_from_cart(
+        db=db,
+        cart_id=cart_id,
+        item_id=remove_request.item_id,
+        quantity=remove_request.quantity,
+    )
+    return {"message": "Item removed from cart successfully"}
