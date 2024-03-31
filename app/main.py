a575a07ce44992d1edc59c8396a5d950d6c542e3
josempd <josempd@gmail.com>
2024-03-31 15:46:14 +0200

    added endpoints to manage carts

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from fastapi import Depends, FastAPI
+from fastapi import Depends, FastAPI, HTTPException
+from fastapi.responses import JSONResponse
 from sqlalchemy.orm import Session
 
 from . import crud, schemas
@@ -21,3 +22,32 @@ async def root():
 def create_item(item: schemas.ItemCreate, db: Session = Depends(get_db)):
     db_item = crud.create_item(db=db, item=item)
     return db_item
+
+
+@app.post("/carts/", response_model=schemas.CartDisplay)
+def create_cart(db: Session = Depends(get_db)):
+    return crud.create_cart(db=db)
+
+
+@app.get("/carts/", response_model=list[schemas.CartDisplay])
+def list_carts(db: Session = Depends(get_db)):
+    carts = crud.get_carts(db=db)
+    return carts
+
+
+@app.get("/carts/{cart_id}", response_model=schemas.CartDisplay)
+def read_cart(cart_id: int, db: Session = Depends(get_db)):
+    db_cart = crud.get_cart(db=db, cart_id=cart_id)
+    if db_cart is None:
+        raise HTTPException(status_code=404, detail="Cart not found")
+    return db_cart
+
+
+@app.delete("/carts/{cart_id}")
+def delete_cart(cart_id: int, db: Session = Depends(get_db)):
+    success = crud.delete_cart(db=db, cart_id=cart_id)
+    if not success:
+        raise HTTPException(status_code=404, detail="Cart not found")
+    return JSONResponse(
+        status_code=200, content={"message": "Cart deleted successfully"}
+    )
