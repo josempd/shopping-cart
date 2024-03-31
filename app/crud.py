cebd9ebda126159191ddd37beec89dc7358b9fb5
josempd <josempd@gmail.com>
2024-03-31 15:45:32 +0200

    add crud operations to manage carts

--- app/crud.py
+++ app/crud.py
@@ -10,3 +10,28 @@ def create_item(db: Session, item: ItemCreate):
     db.commit()
     db.refresh(db_item)
     return db_item
+
+
+def create_cart(db: Session):
+    db_cart = models.Cart()
+    db.add(db_cart)
+    db.commit()
+    db.refresh(db_cart)
+    return db_cart
+
+
+def get_cart(db: Session, cart_id: int):
+    return db.query(models.Cart).filter(models.Cart.id == cart_id).first()
+
+
+def delete_cart(db: Session, cart_id: int):
+    db_cart = db.query(models.Cart).filter(models.Cart.id == cart_id).first()
+    if db_cart:
+        db.delete(db_cart)
+        db.commit()
+        return True
+    return False
+
+
+def get_carts(db: Session):
+    return db.query(models.Cart).all()
