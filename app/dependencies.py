715902fed93297541de9f8830e49500e8547b02b
José Manuel Pires Dias <jmpd@mac.local>
2024-04-03 11:44:18 +0200

    added code lost in refactoring process

--- app/dependencies.py
+++ app/dependencies.py
@@ -1,12 +1,16 @@
 from fastapi import Depends
 from sqlalchemy.orm import Session
-from infrastructure.database import db_session, get_db
+from infrastructure.database import get_db
 from infrastructure.repository import ItemRepository, CartRepository
 from domain.repo_interfaces import ICartRepository
-from domain.service import CartService
+from domain.service import ItemService, CartService
 
 def get_item_repository(db: Session = Depends(get_db)):
     return ItemRepository(db)
+
+def get_item_service(db: Session = Depends(get_db)) -> ItemService:
+    item_repo = ItemRepository(db)
+    return ItemService(item_repo)
 
 def get_cart_repository(db: Session = Depends(get_db)) -> ICartRepository:
     return CartRepository(db)
